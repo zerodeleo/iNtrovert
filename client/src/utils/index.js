03026715cc76832269a9cdd9c3eb27617fb7cba0
zerodeleo <julia.stjerna@hotmail.se>
2022-07-21 10:32:55 +0200

    sorting venueList done, need some fixes, check trello

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -2,7 +2,7 @@ export const sortVenueList = (venueList) => {
     return venueList
       .sort((a, b) => a.busynessNum - b.busynessNum)
       .sort((a, b) => {
-        if(a.busynessNum === b.busynessNum) { // DO NOT CHANGE
+        if(a.busynessNum === b.busynessNum) {
           return a.busynessDeltaNum - b.busynessDeltaNum
         }
         return a.busynessNum
