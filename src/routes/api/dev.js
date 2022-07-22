fab146d45ed7caebc13110a6e697411bbf687cfb
zerodeleo <julia.stjerna@hotmail.se>
2022-07-22 13:26:40 +0200

    venueList is working with multiple types, but missing type in obj

--- src/routes/api/dev.js
+++ src/routes/api/dev.js
@@ -1,60 +1,47 @@
+/* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 /* eslint-disable camelcase */
 const { Router } = require('express');
 require('dotenv').config();
 
 const axios = require('axios');
-const { busynessTxt } = require('./utils');
+const { getGoogleVenues, getBestTimeURLSearchParamsFake, getBestTimeURLSearchParamsFakeOne, combineGoogleAndBesttime, mergeNestedArr } = require('./utils');
 const router = new Router();
 
 const fakeGoogleApiPath = 'http://localhost:5001';
 const fakeBestTimeApiPath = 'http://localhost:5002';
 
-router.route(`/call`).post(async (req, res) => {
+
+router.route(`/call`).get(async (req, res) => {
   const { type, location } = req.body;
 
-  await axios.get(`${fakeGoogleApiPath}/${type}sData`)
-      .then((googleRes) => {
-        const besttimeRes = axios.get(`${fakeBestTimeApiPath}/${type}sData`);
-        const bars = googleRes.data.results.map((result) => {
-          const { name, rating, place_id, vicinity } = result;
-          return { name, rating, place_id, vicinity };
-        });
-        return { besttimeRes, bars };
+  const types = ['parks', 'bars'];
+
+  await axios.get(`${fakeGoogleApiPath}/data`)
+      .then((res) => getGoogleVenues({ types, res }))
+      .then((googleVenues) => ({
+        googleVenues,
+        URLSearchParams: getBestTimeURLSearchParamsFake(googleVenues),
+      }))
+      .then(({ googleVenues, URLSearchParams }) => {
+        const promises = URLSearchParams.map((params) =>
+          axios.get(`${fakeBestTimeApiPath}/data?venue_info.${params}`)
+              .catch((err) => console.error(err)),
+        );
+        return { googleVenues, promises };
       })
-      .then(async (obj) => {
-        return obj.besttimeRes
-            .then((data) => data.data)
-            .then((data) => {
-              const resObj = obj.bars.map((el) => {
-                const venue = data.find((el2) => {
-                  return el2.venue_info.venue_name === el.name;
-                });
-                return {
-                  ...el,
-                  busynessDelta:
-                    venue.analysis.venue_live_forecasted_delta > 0 ?
-                    true :
-                    typeof
-                    venue.analysis.venue_live_forecasted_delta === 'undefined' ?
-                    undefined :
-                    false,
-                  busynessDeltaNum: venue.analysis.venue_live_forecasted_delta,
-                  busynessTxt:
-                    busynessTxt(venue.analysis.venue_forecasted_busyness),
-                  busynessNum: venue.analysis.venue_forecasted_busyness,
-                  createdAt: new Date().getTime(),
-                  id: Math.floor(Math.random() * 10000000000),
-                  type,
-                };
-              });
-              return resObj;
-            })
-            .then((data) => data)
-            .catch((err) => res.status(500).json(err));
+      .then(async ({ googleVenues, promises }) => {
+        const resolvedPromises = await Promise.all(promises);
+        return { googleVenues, resolvedPromises };
       })
-      .then((data) => res.json(data).end())
-      .catch((err) => res.status(500).json(err));
+      .then(({ googleVenues, resolvedPromises }) => {
+        const besttimeVenues = resolvedPromises.map((obj) => obj.data);
+        return { googleVenues, besttimeVenues: mergeNestedArr(besttimeVenues) };
+      })
+      .then(({ googleVenues, besttimeVenues }) => {
+        return combineGoogleAndBesttime({ googleVenues, besttimeVenues });
+      })
+      .then((data) => res.json(data).end());
 });
 
 module.exports = router;
